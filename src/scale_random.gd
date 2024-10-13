d766f3f83bf3edce23fbb2b739a5356f2bb0ea61
Nyanko Nekomi <Nyankonekomi@outlook.com>
2024-10-13 22:59:15 +1100

    commit

--- src/scale_random.gd
+++ src/scale_random.gd
@@ -1,7 +1,7 @@
-extends Sprite
+extends Sprite2D
 
-onready var oscale = scale
+@onready var oscale = scale
 
 func _process(delta):
-	scale = oscale * rand_range(0.8,1.2)
+	scale = oscale * randf_range(0.8,1.2)
 	global_rotation = 0
